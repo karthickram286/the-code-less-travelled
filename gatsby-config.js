fdcebdec4ee7e1e6301224bb82ac9d6351faafec
karthickram286 <karthick.r@zohocorp.com>
2020-01-06 21:22:01 +0530

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,12 @@ module.exports = {
         ]
       }
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-155387404-2",
+      },
+    },
     'gatsby-plugin-offline',
     `gatsby-plugin-react-helmet`,
     'gatsby-plugin-styled-components'
